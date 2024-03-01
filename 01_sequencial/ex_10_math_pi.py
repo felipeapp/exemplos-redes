f859684bb646b2aae7d61d05f4d7f593af3c7881
Felipe A P Pinto <felipe.pinto@ifrn.edu.br>
2024-03-01 13:52:46 -0300

    Exemplos sobre operadores aritméticos e math.

---
+++ 01_sequencial/ex_10_math_pi.py
@@ -0,0 +1,7 @@
+import math
+
+raio = float(input("Digite o valor do raio: "))
+
+area = math.pi * raio**2
+
+print("A área do círculo de raio", raio, "é", area)
---
+++ 01_sequencial/ex_11_math_raiz.py
@@ -0,0 +1,8 @@
+import math
+
+a = float(input("Digite o valor do lado A: "))
+b = float(input("Digite o valor do lado B: "))
+
+h = math.sqrt(a**2 + b**2)
+
+print("A valor do lado H é", h)
