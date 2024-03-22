7ba81cf69042a65cb866ac5ece7134d70cac601b
Felipe A P Pinto <felipe.pinto@ifrn.edu.br>
2024-03-22 19:37:25 -0300

    Exemplos com while e for.

---
+++ 03_repeticao/ex_09_for_colecao.py
@@ -0,0 +1,17 @@
+for i in range(0, 5):
+    print(i)
+
+print(10 * "-")
+
+for i in (0, 1, 2, 3, 4):
+    print(i)
+
+print(10 * "-")
+
+for i in (8, -1, 0, 3.14, 0):
+    print(i)
+
+print(10 * "-")
+
+for i in "Redes de Computadores":
+    print(i)
