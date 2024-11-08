71a24e7e70ae32024f28f770e78129211d4ecbbc
Felipe A P Pinto <felipe.pinto@ifrn.edu.br>
2024-11-08 19:59:12 -0300

    Exemplo calculadora UDP/IP.

---
+++ 02_socket/ex_04_calculadora_cliente_udp.py
@@ -0,0 +1,23 @@
+import socket
+
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+operacao = input("Digite a operação (+, -, *, /): ")
+
+socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+IP_PORTA = ("127.0.0.1", 5000)
+dados = f"{numero1};{numero2};{operacao}".encode()
+
+socket_cliente.sendto(dados, IP_PORTA)
+print("Dados enviados!")
+
+dados, endereco_servidor = socket_cliente.recvfrom(1024)
+resposta = dados.decode()
+
+if resposta == "@":
+    print("O servidor indicou uma operação inválida!")
+else:
+    print(f"Resposta do servidor: {resposta}")
+
+socket_cliente.close()
