40bec68e5bad65e878cf8c041466ab9ff8650d5a
Felipe A P Pinto <felipe.pinto@ifrn.edu.br>
2025-04-23 17:17:12 -0300

    Adiciona exemplos de operações aritméticas, cálculo de média, área de círculo e funções matemáticas

---
+++ 01_sequencial/ex_07_op_aritmeticos.py
@@ -0,0 +1,35 @@
+"""
+Exemplo de programa usando.
+
+operadores aritméticos.
+"""
+
+# Definição de variáveis
+pi = 3.14
+x = 10
+y = 4
+z = 5
+
+# Expressões aritméticas
+a = 2 * x + 4 - z
+b = x + y * y - z
+c = (x + y) * (y - z)
+d = x / 4 + 1  # Divisão real
+e = x // 4 + 1  # Divisão inteira
+f = 2 * y**2
+g = 2 + z % 2
+h = 2 * z % 2
+i = 25 ** (1 / 2)
+j = 27 ** (1 / 3)
+
+# Mostrando os resultados
+print("Variável A =", a)
+print("Variável B =", b)
+print("Variável C =", c)
+print("Variável D =", d)
+print("Variável E =", e)
+print("Variável F =", f)
+print("Variável G =", g)
+print("Variável H =", h)
+print("Variável I =", i)
+print("Variável J =", j)
---
+++ 01_sequencial/ex_08_media_aritmetica.py
@@ -0,0 +1,12 @@
+"""
+Exemplo de script python para cálculo da.
+
+média aritmética entre duas notas.
+"""
+
+nota1 = float(input("Digite sua primeira nota: "))
+nota2 = float(input("Digite sua segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+print("Sua média foi", media)
